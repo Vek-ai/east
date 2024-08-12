d6e9aa1511b579884cfb0bd1c1348a25ac784ac1
Edrian Cadungo <cadungoedrianjoepen@gmail.com>
2024-08-12 17:57:41 +0800

    scan barcode

--- generatebarcode.php
+++ generatebarcode.php
@@ -18,13 +18,8 @@ $barcode = "";
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"> -->
-  <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
-
-  <link rel="stylesheet" href="assets/libs/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/libs/jquery/jquery.js">
-  
-  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> 
 
   <title>Generate Barcode</title>
 
@@ -128,8 +123,6 @@ $barcode = "";
                     
                         // Generate the image source for HTML
                         $img_src = 'data:image/png;base64,' . $barcode_base64;
-
-                        
                     
                         // You can then display the barcode in your HTML like this
                         echo '<img src="' . $img_src . '" alt="UPC Barcode">';
@@ -169,7 +162,6 @@ $barcode = "";
 
 </body>
 
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script> -->
-<script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
 
 </html>
