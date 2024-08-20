0152bda8ad5a399752d22178302d47dbfa232da6
Edrian Cadungo <cadungoedrianjoepen@gmail.com>
2024-08-20 13:51:33 +0800

    remove details in product details

--- pages/backups/Aug 20/product_details.php
+++ pages/backups/Aug 20/product_details.php
@@ -123,11 +123,58 @@ if (!empty($_REQUEST['product_id'])) {
                 <h4 class="fw-semibold mb-3">
                     $<?= $row['unit_price'] ?> 
                 </h4>
-                
+                <div class="d-flex align-items-center gap-8 pb-4 border-bottom">
+                <ul class="list-unstyled d-flex align-items-center mb-0">
+                    <li>
+                    <a class="me-1" href="javascript:void(0)">
+                        <i class="ti ti-star text-warning fs-4"></i>
+                    </a>
+                    </li>
+                    <li>
+                    <a class="me-1" href="javascript:void(0)">
+                        <i class="ti ti-star text-warning fs-4"></i>
+                    </a>
+                    </li>
+                    <li>
+                    <a class="me-1" href="javascript:void(0)">
+                        <i class="ti ti-star text-warning fs-4"></i>
+                    </a>
+                    </li>
+                    <li>
+                    <a class="me-1" href="javascript:void(0)">
+                        <i class="ti ti-star text-warning fs-4"></i>
+                    </a>
+                    </li>
+                    <li>
+                    <a href="javascript:void(0)">
+                        <i class="ti ti-star text-warning fs-4"></i>
+                    </a>
+                    </li>
+                </ul>
+                <a href="javascript:void(0)">(236 reviews)</a>
+                </div>
                 <div class="d-flex align-items-center gap-8 py-7">
                 <h6 class="mb-0 fs-4 fw-semibold">Colors:</h6>
                 <a class="rounded-circle d-block text-bg-primary p-6" href="javascript:void(0)"></a>
                 </div>
+                <div class="d-flex align-items-center gap-7 pb-7 mb-7 border-bottom">
+                <h6 class="mb-0 fs-4 fw-semibold">QTY:</h6>
+                <div class="input-group input-group-sm rounded">
+                    <button class="btn minus min-width-40 py-0 border-end border-muted fs-5 border-end-0 text-muted" type="button" id="add1">
+                    <i class="ti ti-minus"></i>
+                    </button>
+                    <input type="text" class="min-width-40 flex-grow-0 border border-muted text-muted fs-4 fw-semibold form-control text-center qty" placeholder="" aria-label="Example text with button addon" aria-describedby="add1" value="1">
+                    <button class="btn min-width-40 py-0 border border-muted fs-5 border-start-0 text-muted add" type="button" id="addo2">
+                    <i class="ti ti-plus"></i>
+                    </button>
+                </div>
+                </div>
+                <div class="d-sm-flex align-items-center gap-6 pt-8 mb-7">
+                <a href="javascript:void(0)" class="btn d-block btn-primary px-5 py-8 mb-6 mb-sm-0">Buy Now</a>
+                <a href="javascript:void(0)" class="btn d-block btn-danger px-7 py-8">Add to Cart</a>
+                </div>
+                <p class="mb-0">Dispatched in 2-3 weeks</p>
+                <a href="javascript:void(0)">Why the longer time for delivery?</a>
             </div>
             </div>
         </div>
