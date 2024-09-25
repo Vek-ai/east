90a95f31eb86eb8de26f31757681cc7e615e7407
Edrian Cadungo <cadungoedrianjoepen@gmail.com>
2024-09-25 14:40:09 +0800

    Update index.php

--- cashier/index.php
+++ cashier/index.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_COMPILE_WARNING);
 if (!isset($_SESSION['userid'])) {
     $redirect_url = urlencode($_SERVER['REQUEST_URI']);
     header("Location: login.php?redirect=$redirect_url");
