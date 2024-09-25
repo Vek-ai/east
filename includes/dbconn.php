ef5bf072801be7984e391aceaec4216ac49ed393
Edrian Cadungo <cadungoedrianjoepen@gmail.com>
2024-09-25 14:47:52 +0800

    Update dbconn.php

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -3,3 +3,10 @@ $host = "localhost";
 $user = "benguetf_eastkentucky";                
 $password = "O3K9-T6&{oW[";         
 $dbname = "benguetf_eastkentucky";  
+
+$conn = new mysqli("localhost", "username", "password", "database");
+
+// Check for connection errors
+if ($conn->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
