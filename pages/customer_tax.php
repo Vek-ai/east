78875d13d18105f5e5e7ae5227e6edb55382c79c
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-10-18 13:36:02 +0800

    updates on notif, and titles

--- pages/customer_tax.php
+++ pages/customer_tax.php
@@ -9,102 +9,115 @@ $notes = "";
 $saveBtnTxt = "Add";
 $addHeaderTxt = "Add New";
 
-if(!empty($_REQUEST['taxid'])){
+if (!empty($_REQUEST['taxid'])) {
   $taxid = $_REQUEST['taxid'];
   $query = "SELECT * FROM customer_tax WHERE taxid = '$taxid'";
-  $result = mysqli_query($conn, $query);            
+  $result = mysqli_query($conn, $query);
   while ($row = mysqli_fetch_array($result)) {
-      $taxid = $row['taxid'];
-      $tax_status_desc = $row['tax_status_desc'];
-      $percentage = $row['percentage'];
+    $taxid = $row['taxid'];
+    $tax_status_desc = $row['tax_status_desc'];
+    $percentage = $row['percentage'];
   }
   $saveBtnTxt = "Update";
   $addHeaderTxt = "Update";
 }
 
 $message = "";
-if(!empty($_REQUEST['result'])){
-  if($_REQUEST['result'] == '1'){
+if (!empty($_REQUEST['result'])) {
+  if ($_REQUEST['result'] == '1') {
     $message = "New customer tax added successfully.";
     $textColor = "text-success";
-  }else if($_REQUEST['result'] == '2'){
+  } else if ($_REQUEST['result'] == '2') {
     $message = "Customer tax updated successfully.";
     $textColor = "text-success";
-  }else if($_REQUEST['result'] == '0'){
+  } else if ($_REQUEST['result'] == '0') {
     $message = "Failed to Perform Operation";
     $textColor = "text-danger";
   }
-  
+
 }
 
 ?>
 <style>
-        /* Ensure that the text within the notes column wraps properly */
-        td.notes,  td.last-edit{
-            white-space: normal;
-            word-wrap: break-word;
-        }
-        .emphasize-strike {
-            text-decoration: line-through;
-            font-weight: bold;
-            color: #9a841c; /* You can choose any color you like for emphasis */
-        }
-      .dataTables_filter input {
-    width: 100%; /* Adjust the width as needed */
-    height: 50px; /* Adjust the height as needed */
-    font-size: 16px; /* Adjust the font size as needed */
-    padding: 10px; /* Adjust the padding as needed */
-    border-radius: 5px; /* Adjust the border-radius as needed */
-}
-.dataTables_filter {  width: 100%;}
-#toggleActive {
-    margin-bottom: 10px;
-}
+  /* Ensure that the text within the notes column wraps properly */
+  td.notes,
+  td.last-edit {
+    white-space: normal;
+    word-wrap: break-word;
+  }
 
-.inactive-row {
+  .emphasize-strike {
+    text-decoration: line-through;
+    font-weight: bold;
+    color: #9a841c;
+    /* You can choose any color you like for emphasis */
+  }
+
+  .dataTables_filter input {
+    width: 100%;
+    /* Adjust the width as needed */
+    height: 50px;
+    /* Adjust the height as needed */
+    font-size: 16px;
+    /* Adjust the font size as needed */
+    padding: 10px;
+    /* Adjust the padding as needed */
+    border-radius: 5px;
+    /* Adjust the border-radius as needed */
+  }
+
+  .dataTables_filter {
+    width: 100%;
+  }
+
+  #toggleActive {
+    margin-bottom: 10px;
+  }
+
+  .inactive-row {
     display: none;
-}
-    </style>
-    <div class="font-weight-medium shadow-none position-relative overflow-hidden mb-7">
-            <div class="card-body px-0">
-              <div class="d-flex justify-content-between align-items-center">
-                <div><br>
-                  <h4 class="font-weight-medium fs-14 mb-0">Customer Tax</h4>
-                  <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                      <li class="breadcrumb-item">
-                        <a class="text-muted text-decoration-none" href="">Customer
-                        </a>
-                      </li>
-                      <li class="breadcrumb-item text-muted" aria-current="page">Customer Tax</li>
-                    </ol>
-                  </nav>
-                </div>
-                <div>
-                  <div class="d-sm-flex d-none gap-3 no-block justify-content-end align-items-center">
-                    <div class="d-flex gap-2">
-                      <div class="">
-                        <small>This Month</small>
-                        <h4 class="text-primary mb-0 ">$58,256</h4>
-                      </div>
-                      <div class="">
-                        <div class="breadbar"></div>
-                      </div>
-                    </div>
-                    <div class="d-flex gap-2">
-                      <div class="">
-                        <small>Last Month</small>
-                        <h4 class="text-secondary mb-0 ">$58,256</h4>
-                      </div>
-                      <div class="">
-                        <div class="breadbar2"></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  }
+</style>
+<div class="font-weight-medium shadow-none position-relative overflow-hidden mb-7">
+  <div class="card-body px-0">
+    <div class="d-flex justify-content-between align-items-center">
+      <div><br>
+        <h4 class="font-weight-medium fs-14 mb-0">Customer Tax</h4>
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a class="text-muted text-decoration-none" href="">Customer
+              </a>
+            </li>
+            <li class="breadcrumb-item text-muted" aria-current="page">Customer Tax</li>
+          </ol>
+        </nav>
+      </div>
+      <div>
+        <div class="d-sm-flex d-none gap-3 no-block justify-content-end align-items-center">
+          <div class="d-flex gap-2">
+            <div class="">
+              <small>This Month</small>
+              <h4 class="text-primary mb-0 ">$58,256</h4>
+            </div>
+            <div class="">
+              <div class="breadbar"></div>
             </div>
           </div>
+          <div class="d-flex gap-2">
+            <div class="">
+              <small>Last Month</small>
+              <h4 class="text-secondary mb-0 ">$58,256</h4>
+            </div>
+            <div class="">
+              <div class="breadbar2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <div class="col-12">
   <!-- start Default Form Elements -->
   <div class="card card-body">
@@ -116,14 +129,15 @@ if(!empty($_REQUEST['result'])){
         <h4 class="card-title <?= $textColor ?>"><?= $message ?></h4>
       </div>
     </div>
-    
+
 
     <form id="lineForm" class="form-horizontal">
       <div class="row pt-3">
         <div class="col-md-6">
           <div class="mb-3">
             <label class="form-label">Tax Status Description</label>
-            <input type="text" id="tax_status_desc" name="tax_status_desc" class="form-control"  value="<?= $tax_status_desc ?>"/>
+            <input type="text" id="tax_status_desc" name="tax_status_desc" class="form-control"
+              value="<?= $tax_status_desc ?>" />
           </div>
         </div>
         <div class="col-md-6">
@@ -136,17 +150,17 @@ if(!empty($_REQUEST['result'])){
 
       <div class="form-actions">
         <div class="card-body border-top ">
-          <input type="hidden" id="taxid" name="taxid" class="form-control"  value="<?= $taxid ?>"/>
+          <input type="hidden" id="taxid" name="taxid" class="form-control" value="<?= $taxid ?>" />
           <div class="row">
-            
+
             <div class="col-6 text-start">
-            
+
             </div>
             <div class="col-6 text-end">
               <button type="submit" class="btn btn-primary" style="border-radius: 10%;"><?= $saveBtnTxt ?></button>
             </div>
           </div>
-          
+
         </div>
       </div>
 
@@ -158,52 +172,55 @@ if(!empty($_REQUEST['result'])){
   <div class="datatables">
     <div class="card">
       <div class="card-body">
-          <h4 class="card-title d-flex justify-content-between align-items-center">Customer Tax List  &nbsp;&nbsp; <?php if(!empty($_REQUEST['product_line_id'])){ ?>
+        <h4 class="card-title d-flex justify-content-between align-items-center">Customer Tax List &nbsp;&nbsp;
+          <?php if (!empty($_REQUEST['product_line_id'])) { ?>
             <a href="/?page=customer_tax" class="btn btn-primary" style="border-radius: 10%;">Add New</a>
-             <?php } ?> <!-- <div> <input type="checkbox" id="toggleActive" checked> Show Active Only</div> -->
-          </h4>
-        
+          <?php } ?> <!-- <div> <input type="checkbox" id="toggleActive" checked> Show Active Only</div> -->
+        </h4>
+
         <div class="table-responsive">
-       
+
           <table id="display_product_line" class="table table-striped table-bordered text-nowrap align-middle">
             <thead>
               <!-- start row -->
               <tr>
                 <th>Tax Status Description</th>
                 <th>Percentage</th>
-              
+
                 <th>Action</th>
               </tr>
               <!-- end row -->
             </thead>
             <tbody>
-<?php
-$no = 1;
-$query_tax_status_desc = "SELECT * FROM customer_tax";
-$result_tax_status_desc = mysqli_query($conn, $query_tax_status_desc);            
-while ($row_tax_status_desc = mysqli_fetch_array($result_tax_status_desc)) {
-    $taxid = $row_tax_status_desc['taxid'];
-    $tax_status_desc = $row_tax_status_desc['tax_status_desc'];
-    $percentage = $row_tax_status_desc['percentage'];
-?>
-<tr id="customer-tax-row-<?= $no ?>">
-    <td><?= $tax_status_desc ?></td>
-    <td><?= $percentage ?></td>
-    <td class="text-center" id="action-button-<?= $no ?>">
-            <a href="/?page=customer_tax&taxid=<?= $taxid ?>" class="btn btn-primary py-1" style='border-radius: 10%;'>Edit</a>
-            <a class="btn btn-danger py-1 text-light deleteCustomerTax" data-taxid="<?= $taxid ?>" data-row="<?= $no ?>" style='border-radius: 10%;'>Delete</a>
+              <?php
+              $no = 1;
+              $query_tax_status_desc = "SELECT * FROM customer_tax";
+              $result_tax_status_desc = mysqli_query($conn, $query_tax_status_desc);
+              while ($row_tax_status_desc = mysqli_fetch_array($result_tax_status_desc)) {
+                $taxid = $row_tax_status_desc['taxid'];
+                $tax_status_desc = $row_tax_status_desc['tax_status_desc'];
+                $percentage = $row_tax_status_desc['percentage'];
+                ?>
+                <tr id="customer-tax-row-<?= $no ?>">
+                  <td><?= $tax_status_desc ?></td>
+                  <td><?= $percentage ?></td>
+                  <td class="text-center" id="action-button-<?= $no ?>">
+                    <a href="/?page=customer_tax&taxid=<?= $taxid ?>" class="btn btn-primary py-1"
+                      style='border-radius: 10%;'>Edit</a>
+                    <a class="btn btn-danger py-1 text-light deleteCustomerTax" data-taxid="<?= $taxid ?>"
+                      data-row="<?= $no ?>" style='border-radius: 10%;'>Delete</a>
 
-    </td>
-</tr>
-<?php
-$no++;
-}
-?>
-</tbody>
+                  </td>
+                </tr>
+                <?php
+                $no++;
+              }
+              ?>
+            </tbody>
 
 
 
-            
+
           </table>
         </div>
       </div>
@@ -219,7 +236,7 @@ $no++;
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        
+
         <p id="responseMsg"></p>
       </div>
       <div class="modal-footer">
@@ -232,120 +249,120 @@ $no++;
 </div>
 
 <script>
-  $(document).ready(function() {
+  $(document).ready(function () {
     var table = $('#display_product_line').DataTable();
-    
-    $.fn.dataTable.ext.search.push(function(settings, data, dataIndex) {
-        var status = $(table.row(dataIndex).node()).find('a .alert').text().trim();
-        var isActive = $('#toggleActive').is(':checked');
 
-        if (!isActive || status === 'Active') {
-            return true;
-        }
-        return false;
+    $.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
+      var status = $(table.row(dataIndex).node()).find('a .alert').text().trim();
+      var isActive = $('#toggleActive').is(':checked');
+
+      if (!isActive || status === 'Active') {
+        return true;
+      }
+      return false;
     });
 
-    $('#toggleActive').on('change', function() {
-        table.draw();
+    $('#toggleActive').on('change', function () {
+      table.draw();
     });
 
     $('#toggleActive').trigger('change');
 
     function getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
     }
 
-    $('#lineForm').on('submit', function(event) {
-        event.preventDefault(); 
+    $('#lineForm').on('submit', function (event) {
+      event.preventDefault();
 
-        var userid = getCookie('userid');
+      var userid = getCookie('userid');
 
-        var formData = new FormData(this);
-        formData.append('action', 'add_update');
-        formData.append('userid', userid);
+      var formData = new FormData(this);
+      formData.append('action', 'add_update');
+      formData.append('userid', userid);
 
-        var appendResult = "";
+      var appendResult = "";
 
-        $.ajax({
-            url: 'pages/customer_tax_ajax.php',
-            type: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            success: function(response) {
-              
-              if (response === "Customer tax updated successfully.") {
-                  $('#responseHeader').text("Success");
-                  $('#responseMsg').text(response);
-                  $('#responseHeaderContainer').removeClass("bg-danger");
-                  $('#responseHeaderContainer').addClass("bg-success");
-                  $('#response-modal').modal("show");
+      $.ajax({
+        url: 'pages/customer_tax_ajax.php',
+        type: 'POST',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (response) {
 
-                  $('#response-modal').on('hide.bs.modal', function () {
-                    window.location.href = "?page=customer_tax";
-                  });
-              } else if (response === "New customer tax added successfully.") {
-                  $('#responseHeader').text("Success");
-                  $('#responseMsg').text(response);
-                  $('#responseHeaderContainer').removeClass("bg-danger");
-                  $('#responseHeaderContainer').addClass("bg-success");
-                  $('#response-modal').modal("show");
+          if (response === "Customer tax updated successfully.") {
+            $('#responseHeader').text("Success");
+            $('#responseMsg').text(response);
+            $('#responseHeaderContainer').removeClass("bg-danger");
+            $('#responseHeaderContainer').addClass("bg-success");
+            $('#response-modal').modal("show");
 
-                  $('#response-modal').on('hide.bs.modal', function () {
-                      location.reload();
-                  });
-              } else {
-                  $('#responseHeader').text("Failed");
-                  $('#responseMsg').text(response);
+            $('#response-modal').on('hide.bs.modal', function () {
+              window.location.href = "?page=customer_tax";
+            });
+          } else if (response === "New customer tax added successfully.") {
+            $('#responseHeader').text("Success");
+            $('#responseMsg').text(response);
+            $('#responseHeaderContainer').removeClass("bg-danger");
+            $('#responseHeaderContainer').addClass("bg-success");
+            $('#response-modal').modal("show");
 
-                  $('#responseHeaderContainer').removeClass("bg-success");
-                  $('#responseHeaderContainer').addClass("bg-danger");
-                  $('#response-modal').modal("show");
-              }
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                alert('Error: ' + textStatus + ' - ' + errorThrown);
-            }
-        });
+            $('#response-modal').on('hide.bs.modal', function () {
+              location.reload();
+            });
+          } else {
+            $('#responseHeader').text("Failed");
+            $('#responseMsg').text(response);
+
+            $('#responseHeaderContainer').removeClass("bg-success");
+            $('#responseHeaderContainer').addClass("bg-danger");
+            $('#response-modal').modal("show");
+          }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          alert('Error: ' + textStatus + ' - ' + errorThrown);
+        }
+      });
     });
 
-    $(document).on('click', '.deleteCustomerTax', function(event) {
-        event.preventDefault();
-        var taxid = $(this).data('taxid');
-        var row = $(this).data('row');
-        $.ajax({
-            url: 'pages/customer_tax_ajax.php',
-            type: 'POST',
-            data: {
-                taxid: taxid,
-                action: 'delete'
-            },
-            success: function(response) {
-                if (response == "Customer tax deleted successfully.") {
-                  $('#responseHeader').text("Success");
-                  $('#responseMsg').text(response);
-                  $('#responseHeaderContainer').removeClass("bg-danger");
-                  $('#responseHeaderContainer').addClass("bg-success");
-                  $('#response-modal').modal("show");
+    $(document).on('click', '.deleteCustomerTax', function (event) {
+      event.preventDefault();
+      var taxid = $(this).data('taxid');
+      var row = $(this).data('row');
+      $.ajax({
+        url: 'pages/customer_tax_ajax.php',
+        type: 'POST',
+        data: {
+          taxid: taxid,
+          action: 'delete'
+        },
+        success: function (response) {
+          if (response == "Customer tax deleted successfully.") {
+            $('#responseHeader').text("Success");
+            $('#responseMsg').text(response);
+            $('#responseHeaderContainer').removeClass("bg-danger");
+            $('#responseHeaderContainer').addClass("bg-success");
+            $('#response-modal').modal("show");
 
-                  $('#response-modal').on('hide.bs.modal', function () {
-                    window.location.href = "?page=customer_tax";
-                  });
-                } else {
-                    alert('Failed to hide product line.');
-                }
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                alert('Error: ' + textStatus + ' - ' + errorThrown);
-            }
-        });
+            $('#response-modal').on('hide.bs.modal', function () {
+              window.location.href = "?page=customer_tax";
+            });
+          } else {
+            alert('Failed to hide product line.');
+          }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          alert('Error: ' + textStatus + ' - ' + errorThrown);
+        }
+      });
     });
-});
+  });
 </script>
