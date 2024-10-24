d8b009338015ad81618adb2ea7e9404a08c87794
Vek-ai <vekridon@gmail.com>
2024-10-24 09:51:21 +0800

    a

--- pages/customer-dash.php
+++ pages/customer-dash.php
@@ -45,166 +45,68 @@ require 'includes/functions.php';
     </div>
     </div>
 
-    <div class="widget-content searchable-container list">
-
-    <div class="card card-body">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="customer_search" class="form-label">Customer Name</label>
-                        <div class="input-group">
-                            <span class="input-group-text"><i class="fas fa-user"></i></span>
-                            <input class="form-control" placeholder="All Customers" type="text" id="customer_search">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="date_from" class="form-label">Date From</label>
-                        <div class="input-group">
-                            <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
-                            <input type="date" class="form-control" id="date_from">
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="date_to" class="form-label">Date To</label>
-                        <div class="input-group">
-                            <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
-                            <input type="date" class="form-control" id="date_to">
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row mb-3">
-                <div class="col-md-12 text-end">
-                    <button type="button" class="btn btn-primary" id="btn-view-sales">
-                        View Sales
-                    </button>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="datatables">
-                    <div id="tbl-orders" class="product-details table-responsive text-nowrap">
-                        
-                    </div>
-                </div>
-            </div>
+    <div class="row">
+  <!-- text-cards -->
+  <div class="col-lg-3 col-md-6">
+    <div class="card">
+      <div class="card-body p-9">
+        <div class="hstack gap-9">
+          <div class="round-56 rounded-circle text-white d-flex align-items-center justify-content-center text-bg-primary">
+            <i class="ti ti-credit-card fs-6"></i>
+          </div>
+          <div class="align-self-center">
+            <h3 class="mb-1 fs-6">$3249</h3>
+            <span class="text-muted">Total Revenue</span>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-</div>
-
-<div class="modal" id="view_order_details_modal" style="background-color: rgba(0, 0, 0, 0.5);">
-    <div class="modal-dialog modal-xl" role="document">
-        <div class="modal-content p-2">
-            <div class="modal-header">
-                <h6 class="modal-title">Order Details</h6>
-                <button aria-label="Close" class="close" data-bs-dismiss="modal" type="button">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div id="order-details">
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button class="btn ripple btn-secondary" data-bs-dismiss="modal" type="button">Close</button>
-            </div>
+  </div>
+  <div class="col-lg-3 col-md-6">
+    <div class="card">
+      <div class="card-body p-9">
+        <div class="hstack gap-9">
+          <div class="round-56 rounded-circle text-white d-flex align-items-center justify-content-center text-bg-secondary">
+            <i class="ti ti-users fs-6"></i>
+          </div>
+          <div class="align-self-center">
+            <h3 class="mb-1 fs-6">$2376</h3>
+            <span class="text-muted">Online Revenue</span>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
+  <div class="col-lg-3 col-md-6">
+    <div class="card">
+      <div class="card-body p-9">
+        <div class="hstack gap-9">
+          <div class="round-56 rounded-circle text-white d-flex align-items-center justify-content-center text-bg-danger">
+            <i class="ti ti-calendar fs-6"></i>
+          </div>
+          <div class="align-self-center">
+            <h3 class="mb-1 fs-6">$1795</h3>
+            <span class="text-muted">Offline Products</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6">
+    <div class="card">
+      <div class="card-body p-9">
+        <div class="hstack gap-9">
+          <div class="round-56 rounded-circle text-white d-flex align-items-center justify-content-center text-bg-warning">
+            <i class="ti ti-settings fs-6"></i>
+          </div>
+          <div class="align-self-center">
+            <h3 class="mb-1 fs-6">$687</h3>
+            <span class="text-muted">Ad. Expense</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-<script>
-    $("#customer_search").autocomplete({
-        source: function(request, response) {
-            $.ajax({
-                url: "pages/sales_list_ajax.php",
-                type: 'post',
-                dataType: "json",
-                data: {
-                    search_customer: request.term
-                },
-                success: function(data) {
-                    response(data);
-                },
-                error: function(xhr, status, error) {
-                    console.log("Error: " + xhr.responseText);
-                }
-            });
-        },
-        select: function(event, ui) {
-            $('#customer_search').val(ui.item.label);
-            return false;
-        },
-        focus: function(event, ui) {
-            $('#customer_search').val(ui.item.label);
-            return false;
-        },
-        minLength: 0
-    });
 
-    function loadOrderDetails(orderid){
-        $.ajax({
-            url: 'pages/sales_list_ajax.php',
-            type: 'POST',
-            data: {
-                orderid: orderid,
-                fetch_order_details: "fetch_order_details"
-            },
-            success: function(response) {
-                $('#order-details').html(response);
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                alert('Error: ' + textStatus + ' - ' + errorThrown);
-            }
-        });
-    }
-
-    $(document).ready(function() {
-        $('[data-toggle="tooltip"]').tooltip();
-        
-        function performSearch(query) {
-            var customer_name = $('#customer_search').val();
-            var date_from = $('#date_from').val();
-            var date_to = $('#date_to').val();
-
-            if(!date_from && !date_to){
-                alert('Please select a start and end date!');
-            } else {
-                $.ajax({
-                    url: 'pages/sales_list_ajax.php',
-                    type: 'POST',
-                    data: {
-                        customer_name: customer_name,
-                        date_from: date_from,
-                        date_to: date_to,
-                        search_orders: 'search_orders'
-                    },
-                    success: function(response) {
-                        $('#tbl-orders').html(response);
-                    },
-                    error: function(jqXHR, textStatus, errorThrown) {
-                        alert('Error: ' + textStatus + ' - ' + errorThrown);
-                    }
-                });
-            }
-        }
-
-        $('#btn-view-sales').on('click', function() {
-            performSearch();
-        });
-
-        $(document).on('click', '#view_order_details', function(event) {
-            var orderid = $(this).data('id');
-            loadOrderDetails(orderid);
-            $('#view_order_details_modal').modal('toggle');
-        });
-    });
-</script>
+  </div>
