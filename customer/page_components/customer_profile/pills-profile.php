c23b522dc441d167fde0cce5b19521a2a33e08e0
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-10-30 11:12:53 +0800

    Customer profile page update

--- customer/page_components/customer_profile/pills-profile.php
+++ customer/page_components/customer_profile/pills-profile.php
@@ -2,17 +2,18 @@
   <div class="col-lg-12 mt-4">
     <div class="card shadow-none">
       <div class="card-body">
+
         <div class="row pt-3">
           <div class="col-md-6">
             <div class="mb-3">
-              <label class="form-label">First Name</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['customer_first_name']) ?></span>
+              <label class="form-label">First Name:</label>
+              <span id="customer_first_name"><?= htmlspecialchars($currentUser['customer_first_name']) ?></span>
             </div>
           </div>
           <div class="col-md-6">
             <div class="mb-3">
-              <label class="form-label">Last Name</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['customer_last_name']) ?></span>
+              <label class="form-label">Last Name:</label>
+              <span id="customer_last_name"><?= htmlspecialchars($currentUser['customer_last_name']) ?></span>
             </div>
           </div>
         </div>
@@ -20,20 +21,20 @@
         <div class="row pt-3">
           <div class="col-md-4">
             <div class="mb-3">
-              <label class="form-label">Customer Email</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['contact_email']) ?></span>
+              <label class="form-label">Customer Email:</label>
+              <span id="contact_email"><?= htmlspecialchars($currentUser['contact_email']) ?></span>
             </div>
           </div>
           <div class="col-md-4">
             <div class="mb-3">
-              <label class="form-label">Customer Phone</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['contact_phone']) ?></span>
+              <label class="form-label">Customer Phone:</label>
+              <span id="contact_phone"><?= htmlspecialchars($currentUser['contact_phone']) ?></span>
             </div>
           </div>
           <div class="col-md-4">
             <div class="mb-3">
-              <label class="form-label">Customer Fax</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['contact_fax']) ?></span>
+              <label class="form-label">Customer Fax:</label>
+              <span id="contact_fax"><?= htmlspecialchars($currentUser['contact_fax']) ?></span>
             </div>
           </div>
         </div>
@@ -41,18 +42,8 @@
         <div class="row pt-3">
           <div class="col-md-12">
             <div class="mb-3">
-              <label class="form-label">Customer Business Name</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['customer_business_name']) ?></span>
-            </div>
-          </div>
-        </div>
-
-        <!-- Tax Status and Address Information -->
-        <div class="row pt-3">
-          <div class="col-md-6">
-            <div class="mb-3">
-              <label class="form-label">Tax Exempt Number</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['tax_exempt_number']) ?></span>
+              <label class="form-label">Customer Business Name:</label>
+              <span id="customer_business_name"><?= htmlspecialchars($currentUser['customer_business_name']) ?></span>
             </div>
           </div>
         </div>
@@ -60,8 +51,29 @@
         <div class="row pt-3">
           <div class="col-md-12">
             <div class="mb-3">
-              <label class="form-label">Address</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['address']) ?></span>
+              <label class="form-label">Address:</label>
+              <span id="address"><?= htmlspecialchars($currentUser['address']) ?></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="row pt-3">
+          <div class="col-md-4">
+            <div class="mb-3">
+              <label class="form-label">City:</label>
+              <span id="city"><?= htmlspecialchars($currentUser['city']) ?></span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="mb-3">
+              <label class="form-label">State:</label>
+              <span id="state"><?= htmlspecialchars($currentUser['state']) ?></span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="mb-3">
+              <label class="form-label">Zip:</label>
+              <span id="zip"><?= htmlspecialchars($currentUser['zip']) ?></span>
             </div>
           </div>
         </div>
@@ -69,20 +81,35 @@
         <div class="row pt-3">
           <div class="col-md-6">
             <div class="mb-3">
-              <label class="form-label">City</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['city']) ?></span>
+              <label class="form-label">Secondary Contact Name:</label>
+              <span id="secondary_contact_name"><?= htmlspecialchars($currentUser['secondary_contact_name']) ?></span>
             </div>
           </div>
           <div class="col-md-6">
             <div class="mb-3">
-              <label class="form-label">State</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['state']) ?></span>
+              <label class="form-label">Secondary Contact Phone:</label>
+              <span id="secondary_contact_phone"><?= htmlspecialchars($currentUser['secondary_contact_phone']) ?></span>
             </div>
           </div>
-          <div class="col-md-6">
+        </div>
+
+        <div class="row pt-3">
+          <div class="col-md-4">
             <div class="mb-3">
-              <label class="form-label">Zip</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['zip']) ?></span>
+              <label class="form-label">AP Contact Name:</label>
+              <span id="ap_contact_name"><?= htmlspecialchars($currentUser['ap_contact_name']) ?></span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="mb-3">
+              <label class="form-label">AP Contact Email:</label>
+              <span id="ap_contact_email"><?= htmlspecialchars($currentUser['ap_contact_email']) ?></span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="mb-3">
+              <label class="form-label">AP Contact Phone:</label>
+              <span id="ap_contact_phone"><?= htmlspecialchars($currentUser['ap_contact_phone']) ?></span>
             </div>
           </div>
         </div>
@@ -90,27 +117,21 @@
         <div class="row pt-3">
           <div class="col-md-6">
             <div class="mb-3">
-              <label class="form-label">Secondary Contact Name</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['secondary_contact_name']) ?></span>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="mb-3">
-              <label class="form-label">Secondary Contact Phone</label>
-              <span class="form-control"><?= htmlspecialchars($currentUser['secondary_contact_phone']) ?></span>
+              <label class="form-label">Tax Exempt Number:</label>
+              <span id="tax_exempt_number"><?= htmlspecialchars($currentUser['tax_exempt_number']) ?></span>
             </div>
           </div>
         </div>
 
         <div class="mb-3">
-          <label class="form-label">Customer Notes</label>
-          <p class="form-control"><?= nl2br(htmlspecialchars($currentUser['customer_notes'])) ?></p>
+          <label class="form-label">Customer Notes:</label>
+          <span id="customer_notes"><?= nl2br(htmlspecialchars($currentUser['customer_notes'])) ?></span>
         </div>
 
         <div class="row mb-3">
           <div class="col">
-            <label class="form-label">Customer Call Status</label>
-            <input type="checkbox" disabled <?= $currentUser['call_status'] ? 'checked' : '' ?>>
+            <label class="form-label">Customer Call Status:</label>
+            <span id="call_status"><?= $currentUser['call_status'] ? 'Yes' : 'No' ?></span>
           </div>
         </div>
 
