93c6a150dd2ad29fa49df0116cbe7fe0af0b8637
Edrian Cadungo <cadungoedrianjoepen@gmail.com>
2024-11-08 17:12:58 +0800

    Update maps.php

--- maps.php
+++ maps.php
@@ -14,15 +14,63 @@
         }
     </style>
 </head>
-<body>
+<body class="d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="container align-items-center">
+        <div class="d-flex mb-3 align-items-center">
+            <div class="mr-2">
+                <h5>Address 1:</h5>
+            </div>
+            <div class="mr-2">
+                <input id="searchBox1" class="form-control" placeholder="Enter a location" style="width: 250px;">
+            </div>
+            <div>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#map1Modal">
+                    Choose From Map
+                </button>
+            </div>
+        </div>
 
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#map1Modal">
-        Open Map 1
-    </button>
+        <div class="d-flex mb-3 align-items-center">
+            <div class="mr-2">
+                <h5>Address 2:</h5>
+            </div>
+            <div class="mr-2">
+                <input id="searchBox2" class="form-control" placeholder="Enter a location" style="width: 250px;">
+            </div>
+            <div>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#map2Modal">
+                    Choose From Map
+                </button>
+            </div>
+        </div>
 
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#map2Modal">
-        Open Map 2
-    </button>
+        <div class="d-flex mb-3 align-items-center">
+            <div class="mr-2">
+                <h5>Distance:</h5>
+            </div>
+            <div>
+                <span id="distance">10</span> Miles
+            </div>
+        </div>
+
+        <div class="d-flex mb-3 align-items-center">
+            <div class="mr-2">
+                <h5>Amount Per Mile:</h5>
+            </div>
+            <div class="mr-2">
+                <input id="amountPerMile" class="form-control" placeholder="Enter amount per mile" style="width: 250px;">
+            </div>
+        </div>
+
+        <div class="d-flex mb-3 align-items-center">
+            <div class="mr-2">
+                <h5>Shipping Amount:</h5>
+            </div>
+            <div class="mr-2">
+                <span id="shippingAmount">$0.00</span>
+            </div>
+        </div>
+    </div>
 
     <div class="modal fade" id="map1Modal" tabindex="-1" role="dialog" aria-labelledby="mapsModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
@@ -60,6 +108,24 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
     
     <script>
+        $(document).ready(function() {
+            $('#amountPerMile').on('input', function() {
+                var distance = parseFloat($('#distance').text());
+                var amountPerMile = parseFloat($(this).val());
+
+                if (isNaN(distance)) {
+                    distance = 0;
+                }
+
+                if (!isNaN(amountPerMile)) {
+                    var shippingAmount = distance * amountPerMile;
+                    $('#shippingAmount').text('$' + shippingAmount.toFixed(2));
+                } else {
+                    $('#shippingAmount').text('$0.00');
+                }
+            });
+        });
+
         let map1, map2;
         let marker1, marker2;
         let lat1, lng1, lat2, lng2;
@@ -73,6 +139,40 @@
             map2 = new google.maps.Map(document.getElementById("map2"), {
                 center: { lat: 34.0522, lng: -118.2437 },
                 zoom: 10,
+            });
+
+            const searchBox1 = new google.maps.places.Autocomplete(document.getElementById("searchBox1"));
+            const searchBox2 = new google.maps.places.Autocomplete(document.getElementById("searchBox2"));
+
+            // Automatically initialize the autocomplete for the inputs
+            searchBox1.addListener("place_changed", function() {
+                const place = searchBox1.getPlace();
+                if (!place.geometry || !place.geometry.location) return;
+                if (marker1) marker1.setMap(null);
+                marker1 = new google.maps.Marker({
+                    position: place.geometry.location,
+                    map: map1,
+                    title: place.name,
+                });
+                map1.setCenter(place.geometry.location);
+                // Log the latitude and longitude of the selected place
+                console.log('Start Point Latitude: ' + place.geometry.location.lat());
+                console.log('Start Point Longitude: ' + place.geometry.location.lng());
+            });
+
+            searchBox2.addListener("place_changed", function() {
+                const place = searchBox2.getPlace();
+                if (!place.geometry || !place.geometry.location) return;
+                if (marker2) marker2.setMap(null);
+                marker2 = new google.maps.Marker({
+                    position: place.geometry.location,
+                    map: map2,
+                    title: place.name,
+                });
+                map2.setCenter(place.geometry.location);
+                // Log the latitude and longitude of the selected place
+                console.log('End Point Latitude: ' + place.geometry.location.lat());
+                console.log('End Point Longitude: ' + place.geometry.location.lng());
             });
 
             google.maps.event.addListener(map1, 'click', function(event) {
@@ -114,21 +214,24 @@
 
         function loadGoogleMapsAPI() {
             const script = document.createElement('script');
-            script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDFpFbxFFK7-daOKoIk9y_GB4m512Tii8M&callback=initMaps&libraries=geometry';
+            script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDFpFbxFFK7-daOKoIk9y_GB4m512Tii8M&callback=initMaps&libraries=geometry,places';
             script.async = true;
             script.defer = true;
             document.head.appendChild(script);
         }
 
+        // Load the Google Maps API immediately on page load
+        window.onload = loadGoogleMapsAPI;
+
         $('#map1Modal').on('shown.bs.modal', function () {
             if (!map1) {
-                loadGoogleMapsAPI();
+                initMaps(); // Ensure map1 is initialized when modal opens
             }
         });
 
         $('#map2Modal').on('shown.bs.modal', function () {
             if (!map2) {
-                loadGoogleMapsAPI();
+                initMaps(); // Ensure map2 is initialized when modal opens
             }
         });
 
@@ -137,7 +240,7 @@
             const point2 = new google.maps.LatLng(lat2, lng2);
             const distanceInMeters = google.maps.geometry.spherical.computeDistanceBetween(point1, point2);
             const distanceInMiles = distanceInMeters / 1609.34;
-            alert("Distance between the points: " + distanceInMiles.toFixed(2) + " miles");
+            $('#distance').text(distanceInMiles.toFixed(2));
         }
     </script>
 
