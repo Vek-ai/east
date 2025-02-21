f586c4b5c3a2eecef8189d34f479b849f9cdb1d5
Edrian Cadungo <cadungoedrianjoepen@gmail.com>
2025-02-21 10:57:50 +0800

    Update add_color_trim.php

--- pages/add_color_trim.php
+++ pages/add_color_trim.php
@@ -18,7 +18,50 @@ if(isset($_REQUEST['action'])) {
             $row = mysqli_fetch_assoc($result);
         }
         ?>
-
+            <div class="col-md-6 panel-fields" data-id="7">
+                <label class="form-label">Color Group</label>
+                <div class="mb-3">
+                    <select id="color" class="form-control calculate" name="color">
+                        <option value="">Select Color...</option>
+                        <?php
+                        $query_color_group = "SELECT * FROM color_group_name WHERE hidden = '0' ORDER BY color_group_name";
+                        $result_color_group = mysqli_query($conn, $query_color_group);
+                        while ($row_color_group = mysqli_fetch_array($result_color_group)) {
+                            $selected = (($row['color'] ?? '') == $row_color_group['color_group_name_id']) ? 'selected' : '';
+                        ?>
+                            <option value="<?= $row_color_group['color_group_name_id'] ?>" <?= $selected ?>><?= $row_color_group['color_group_name'] ?></option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-6 mb-3 panel-fields" data-id="7">
+                <label class="form-label">Color Multiplier Value</label>
+                <input type="text" class="form-control" name="coating" id="coating" value="<?=$row['coating'] ?? ''?>">
+            </div>
+            <div class="col-md-6 panel-fields" data-id="7">
+                <label class="form-label">Coating</label>
+                <div class="mb-3">
+                    <select id="color" class="form-control calculate" name="color">
+                        <option value="">Select Color...</option>
+                        <?php
+                        $query_product_coating = "SELECT * FROM product_coating WHERE hidden = '0' ORDER BY product_coating";
+                        $result_product_coating = mysqli_query($conn, $query_product_coating);
+                        while ($row_product_coating = mysqli_fetch_array($result_product_coating)) {
+                            $selected = (($row['coating'] ?? '') == $row_product_coating['product_coating_id']) ? 'selected' : '';
+                        ?>
+                            <option value="<?= $row_product_coating['product_coating_id'] ?>" <?= $selected ?>><?= $row_product_coating['product_coating'] ?></option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-6 mb-3 panel-fields" data-id="7">
+                <label class="form-label">Coating Multiplier Value</label>
+                <input type="text" class="form-control" name="coating_multiplier" id="coating_multiplier" value="<?=$row['coating_multiplier'] ?? ''?>">
+            </div>
         <?php
         
     } 
