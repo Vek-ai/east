b43da7e39355bd4cc2df09d279a562405969bad3
dr4g0n100 <74046398+dr4g0n100@users.noreply.github.com>
2025-04-08 19:44:07 +0800

    show error for debugging

--- customer/pages/login_ajax.php
+++ customer/pages/login_ajax.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-error_reporting(E_ALL & ~E_DEPRECATED);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require '../../includes/dbconn.php';
 require '../../includes/functions.php';
