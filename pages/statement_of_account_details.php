7c3323fcdf13298d6241a08b50e2cc00ec153788
Edrian Cadungo <cadungoedrianjoepen@gmail.com>
2025-07-04 09:27:20 +0800

    Update statement_of_account_details.php

--- pages/statement_of_account_details.php
+++ pages/statement_of_account_details.php
@@ -347,7 +347,7 @@ if(isset($_REQUEST['customer_id'])){
                                         <td class="text-end"><?= $payments > 0 ? '$' .number_format($payments, 2) : '' ?></td>
                                         <td class="text-end"><?= $credit > 0 ? '$' .number_format($credit, 2) : '' ?></td>
                                         <td class="text-end">
-                                            <?= $credit > 0 ? '$' .number_format($balance, 2) : '' ?>
+                                            <?= $balance > 0 ? '$' .number_format($balance, 2) : '' ?>
                                         </td>
                                         <td>
                                             <?php
