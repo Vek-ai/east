a6340593e636b1be5f0f8539d5ad7917db185ffb
dr4g0n100 <74046398+dr4g0n100@users.noreply.github.com>
2025-08-27 17:34:05 +0800

    Update cashier_ajax.php

--- cashier3/pages/cashier_ajax.php
+++ cashier3/pages/cashier_ajax.php
@@ -49,7 +49,12 @@ if (isset($_POST['modifyquantity']) || isset($_POST['duplicate_product'])) {
 
     $key = false;
     foreach ($_SESSION['cart'] as $k => $item) {
-        if ($item['product_id'] == $product_id) {
+        if (
+            $item['product_id'] == $product_id &&
+            (int)($item['custom_grade'] ?? 0) == $grade_id &&
+            (int)($item['custom_profile'] ?? 0) == $gauge_id &&
+            (int)($item['custom_color'] ?? 0) == $color_id
+        ) {
             $key = $k;
             break;
         }
