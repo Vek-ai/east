30ceb587b983dc422cdc227154185174aaa6061c
dr4g0n100 <74046398+dr4g0n100@users.noreply.github.com>
2026-01-15 18:51:37 +0800

    Update print_estimate_product.php

--- print_estimate_product.php
+++ print_estimate_product.php
@@ -733,11 +733,11 @@ if (mysqli_num_rows($result) > 0) {
         $leftBottom = $pdf->GetY();
 
         $deliver_name = trim(
-            ($row_estimatess['deliver_fname'] ?? '') . ' ' . ($row_estimatess['deliver_lname'] ?? '')
+            ($row_estimates['deliver_fname'] ?? '') . ' ' . ($row_estimates['deliver_lname'] ?? '')
         );
 
         if ($deliver_name === '') {
-            $deliver_name = get_customer_name($row_estimatess['customerid']);
+            $deliver_name = get_customer_name($row_estimates['customerid']);
         }
         $rightText = trim($deliver_name) . "\n";
         $shipAddressParts = [];
